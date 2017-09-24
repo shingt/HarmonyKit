3f083a711adcc434b9e6611d3e021d1cc0fb8256
shingt <shingtshin@gmail.com>
2017-09-24 19:01:56 +0900

    Change tone so that it gets compared based on order

--- Sources/Tone.swift
+++ Sources/Tone.swift
@@ -13,8 +13,28 @@ public enum Tone: String {
     case Ab
 }
 
+public extension Tone {
+    // Even though each tone is represented in String, we still need order of each tone.
+    public var order: Int {
+        switch self {
+        case .A:  return 0
+        case .Bb: return 1
+        case .B:  return 2
+        case .C:  return 3
+        case .Db: return 4
+        case .D:  return 5
+        case .Eb: return 6
+        case .E:  return 7
+        case .F:  return 8
+        case .Gb: return 9
+        case .G:  return 10
+        case .Ab: return 11
+        }
+    }
+}
+
 extension Tone: Comparable {
     public static func < (lhs: Tone, rhs: Tone) -> Bool {
-        return lhs.rawValue < rhs.rawValue
+        return lhs.order < rhs.order
     }
 }
